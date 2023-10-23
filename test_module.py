4f8006afb016488b4b9a01ef007d6b1685cde22a
Andrey Mishin <mishin23042002@gmail.com>
2023-10-24 01:23:12 +0300

    Внез изменения в файл test_modules.py добавил корректные отступы

--- test_module.py
+++ test_module.py
@@ -1,13 +1,13 @@
 import os
 
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
 
 files_list = ['main.py', 'readme.md']
+mess = ''
 
 
 def test_program():
     for filename in files_list:
-        assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
-
+        mess = (f'Файл `{filename}` не найден в корне репозитория')
+        assert filename in dir_files, mess
